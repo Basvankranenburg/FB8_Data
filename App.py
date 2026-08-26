2907ac5f55196b96af58105ba00f4df4ff5a0332
Bas van Kranenburg <baskranenburg110779@gmail.com>
2026-08-26 10:00:17 +0200

    Updated App.py and requirements

--- App.py
+++ App.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 from streamlit_gsheets import GSheetsConnection
 
+#TEST TEST ETST 
+
 # 1. Page Config MUST be the first Streamlit command
 st.set_page_config(page_title="Squad Minutes Tracker", layout="wide")
 
